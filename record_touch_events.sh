da9ba8f55bc34e4e3750f2215feb1f6709b64be7
tribta <joaocartuchoo@gmail.com>
2019-09-25 22:12:50 +0100

    check android version for recording

--- record_touch_events.sh
+++ record_touch_events.sh
@@ -2,6 +2,8 @@
 
 echo "Looking for touchscreen device..."
 TOUCH_DEVICE=`./find_touchscreen_name.sh`
+ANDROID_VERSION=`adb shell getprop ro.build.version.release`
+MIN_VERSION=6.0
 
 echo "$TOUCH_DEVICE"
 
@@ -11,7 +13,12 @@ then
     # Device found! Start recording
     echo "Recording will start as soon as you put your finger in the touchscreen."
     echo "Press ctrl+c to stop recording..."
-    
-    #exec-out is shell without buffering, fixing missing last touch data event
-    `adb exec-out getevent -t "${TOUCH_DEVICE#*-> }" > recorded_touch_events.txt` 
+
+    if (( $(echo $ANDROID_VERSION ">" $MIN_VERSION| tr -d $'\r' | bc -l) )); then
+        #exec-out is shell without buffering, fixing missing last touch data event
+        adb exec-out getevent -t "${TOUCH_DEVICE#*-> }" > recorded_touch_events.txt
+    else
+        # if Android version <= 6.0
+        adb shell getevent -t "${TOUCH_DEVICE#*-> }" > recorded_touch_events.txt
+    fi
 fi
