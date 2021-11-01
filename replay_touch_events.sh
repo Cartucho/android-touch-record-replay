ef88b9668942388f9fa3f0e81d7b4c31e59093d0
Nutriz <jerome.gully0@gmail.com>
2021-11-01 23:25:51 +0100

    Updated replay script to handle x86 emulator as argument

--- replay_touch_events.sh
+++ replay_touch_events.sh
@@ -2,16 +2,18 @@
 
 echo "Looking for touchscreen device..."
 TOUCH_DEVICE=`./find_touchscreen_name.sh`
-
 echo "$TOUCH_DEVICE"
 
-# Check if the file `mysendevent` exists in the phone
+# detect "-emulator" argument for emulator
+BINARY_NAME="mysendevent"
+if [[ "$1" == "-emulator" ]]
+then
+   BINARY_NAME+="-x86"
+fi
 
-MYSENDEVENT=`adb shell ls /data/local/tmp/mysendevent 2>&1`
-echo ---"$MYSENDEVENT"---
-[[ "$MYSENDEVENT" == *"No such file or directory"* ]] && adb push mysendevent /data/local/tmp/
-
+# push binary and touch events
+adb push ${BINARY_NAME} /data/local/tmp/
 adb push recorded_touch_events.txt /sdcard/
 
 # Replay the recorded events
-adb shell /data/local/tmp/mysendevent "${TOUCH_DEVICE#*-> }" /sdcard/recorded_touch_events.txt
+adb shell /data/local/tmp/"${BINARY_NAME}" "${TOUCH_DEVICE#*-> }" /sdcard/recorded_touch_events.txt
